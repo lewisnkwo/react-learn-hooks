eaf28db73f7c1826fa99cf106bbf9db67aa67c4f
Lewis Nkwo <lewisnkwo@live.co.uk>
2020-08-12 11:03:05 +0100

    Learning away!

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+interface Note {
+  title: string;
+}
+
+interface Body<T> {
+  content: T;
+}
+
+const NoteApp = () => {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [title, setTitle] = useState("");
+  const [bodyContent, setbodyContent] = useState<Body<Note>>();
+
+  const addNote = (e: any) => {
+    e.preventDefault();
+    setNotes([...notes, { title }]);
+    setTitle("");
+  };
+
+  const removeNote = (title: string) => {
+    setNotes(notes.filter((note) => note.title !== title));
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {notes.map((note) => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <button onClick={() => removeNote(note.title)}>x</button>
+        </div>
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)}></input>
+        <button>add note</button>
+        <input value={title} onChange={(e) => setTitle(e.target.value)}></input>
+        <button>add body</button>
+      </form>
+    </div>
+  );
+};
+
+ReactDOM.render(<NoteApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
